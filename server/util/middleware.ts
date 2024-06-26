42bf55d5fb2475da0996ebf89cefbfd825b9da0c
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-26 17:29:24 +0300

    Add JWT token extraction middleware

--- server/util/middleware.ts
+++ server/util/middleware.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import { z } from 'zod';
+import jwt, { Secret } from 'jsonwebtoken';
+import config from './config';
+import db from './db';
 
 export function validate(schema: z.ZodSchema<any>) {
   return (req: Request, res: Response, next: NextFunction) => {
@@ -18,4 +21,25 @@ export function validate(schema: z.ZodSchema<any>) {
         }
     }
   };
+}
+
+interface Req extends Request {
+  token?: object
+}
+
+export function extractToken(req: Req, res: Response, next: NextFunction) {
+  const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      try {
+        const decodedToken = jwt.verify(authorization.substring(7), config.SECRET as Secret)
+        if (typeof decodedToken === 'object') {
+          req.token = decodedToken
+        }
+      } catch{
+        return res.status(401).json({ error: 'token invalid' })
+      }
+    }  else {
+      return res.status(401).json({ error: 'token missing' })
+    }
+    next()
 }
