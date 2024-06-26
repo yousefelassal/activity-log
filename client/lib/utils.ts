fba6f5ea6d4a40feddc1c22d26cd95179b0a2fae
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-26 22:42:24 +0300

    feat: Add cn utility function for merging CSS class names

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,2 +1,8 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export const baseUrl = process.env.BASE_URL || "http://localhost:3001/api";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
