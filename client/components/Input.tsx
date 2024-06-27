5db1197eabfac936833681baf797760889465bc9
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-27 17:00:28 +0300

    Create Input component

---
+++ client/components/Input.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+
+const Input = ({ className, type, ...props }: React.InputHTMLAttributes<HTMLInputElement>) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        "flex h-[45px] w-full text-[14px] font-normal border border-[#E0E0E0] bg-transparent text-primaryText text-sm rounded-lg p-3 placeholder:text-[#959595] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#959595] transition-all",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export default Input
