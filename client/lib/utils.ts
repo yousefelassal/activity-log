16f722881a2613436d32c128f5aca9aea0875761
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-27 14:28:34 +0300

    Add formatDate function for formatting dates

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -6,3 +6,14 @@ export const baseUrl = process.env.BASE_URL || "http://localhost:3001/api";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: Date) {
+    const options: Intl.DateTimeFormatOptions = {
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    };
+    return date.toLocaleDateString('en-US', options);
+}
