ee93ca86774c702be4c743491969549e9d6eb4a3
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-27 14:59:25 +0300

    Update utils.ts to include a function for generating gradients

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -17,3 +17,24 @@ export function formatDate(date: Date) {
     };
     return date.toLocaleDateString('en-US', options);
 }
+
+const gradients: Record<string, string> = {}
+
+const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+export const getGradient = (name: string) => {
+    if (!gradients[name]) {
+        const color1 = getRandomColor();
+        const color2 = getRandomColor();
+        const gradient = `linear-gradient(139deg, ${color1} 14.17%, ${color2} 84.99%)`;
+        gradients[name] = gradient;
+    }
+    return gradients[name];
+}
