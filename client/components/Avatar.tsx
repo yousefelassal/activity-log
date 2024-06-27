d6ab5877dc55180b98e7bad2e219be5f84c2930b
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-27 18:04:55 +0300

    refactor: Update Avatar component to accept a className prop

--- client/components/Avatar.tsx
+++ client/components/Avatar.tsx
@@ -1,9 +1,13 @@
+import { cn } from "@/lib/utils"
 import { getGradient } from "@/lib/utils"
 
-const Avatar = ({ name }:{ name: string }) => {
+const Avatar = ({ className, name }:{ className?:string, name: string }) => {
   const gradient = getGradient(name)
   return (
-    <div className="rounded-full h-[25px] w-[25px] text-white text-[12px] font-bold uppercase flex items-center justify-center" style={{background: gradient}}>
+    <div
+      className={cn("rounded-full h-[25px] w-[25px] text-white text-[12px] font-bold uppercase flex items-center justify-center", className)}
+      style={{background: gradient}}
+    >
         {name.charAt(0)}
     </div>
   )
