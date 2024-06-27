8e8ceaa1fa8ac4813107761ddfbe1a730a60dc5c
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-27 23:30:59 +0300

    feat: Update Row component to display target information

--- client/components/Row.tsx
+++ client/components/Row.tsx
@@ -91,9 +91,26 @@ const Row = ({ event }: { event: AllEvents }) => {
                             Metadata
                         </span>
                         {isLoading ? (
-                            <div className="h-[144px] w-[360px] rounded-sm animate-pulse bg-[#F8F8F8]"></div>
+                            <div className="h-[144px] w-[300px] rounded-sm animate-pulse bg-[#F8F8F8]"></div>
                         ) : (
-                            <pre className="min-w-max">{JSON.stringify(data?.metadata, null, 2)}</pre>
+                            <pre className="w-[300px] overflow-auto">{JSON.stringify(data?.metadata, null, 2)}</pre>
+                            )}
+                    </div>
+                    <div className="grid grid-cols-[40px_1fr] items-start justify-start h-fit gap-y-3 gap-x-8">
+                        <span className="col-span-2 pb-2 uppercase font-medium text-[14px] text-[#929292]">
+                            Target
+                        </span>
+                        <span className="text-[14px] text-[#929292]">ID</span>
+                        {isLoading ? (
+                            <div className="h-[14px] w-[150px] rounded-sm animate-pulse bg-[#F8F8F8]"></div>
+                        ) : (
+                            <span className="text-[14px] text-black">{data?.target_id}</span>
+                            )}
+                        <span className="text-[14px] text-[#929292]">Name</span>
+                        {isLoading ? (
+                            <div className="h-[14px] w-[150px] rounded-sm animate-pulse bg-[#F8F8F8]"></div>
+                        ) : (
+                            <span className="text-[14px] text-black">{data?.target_name}</span>
                             )}
                     </div>
                 </div>
