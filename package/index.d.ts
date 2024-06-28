fdf92734fbbbe6fc15ef310f7058b3ec73c899d4
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-28 15:19:04 +0300

    Add InstaLog types def declaration file

---
+++ package/index.d.ts
@@ -0,0 +1,43 @@
+declare module 'InstaLog' {
+    interface EventObject {
+        actor_id: string,
+        action: {
+            id: string,
+            object: string,
+            name: string
+        },
+        object: string,
+        target_id: string,
+        target_name: string,
+        location: string,
+        metadata: {
+            redirect: string,
+            description: string,
+            x_request_id: string
+        }
+    }
+
+    interface EventResponse {
+        id: string,
+        actor: {
+            email: string,
+            name: string
+        },
+        action_name: string,
+        occurred_at: string
+    }
+
+    interface ListParams {
+        page: number,
+        limit: number,
+        search: string
+    }
+
+    class InstaLog {
+        constructor(secretKey: string);
+        createEvent(eventObject: EventObject): Promise<EventResponse>;
+        listEvents(params?: ListParams): Promise<EventResponse[]>;
+    }
+
+    export = InstaLog;
+}
