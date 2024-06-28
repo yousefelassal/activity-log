81ab1a26ed7c3e532b64b453455c2b9f3a88cc0d
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-28 12:04:59 +0300

    Create Button component

---
+++ client/components/Button.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils"
+
+const Button = ({ children, className, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+        className={cn("flex items-center justify-center gap-1  h-[45px] px-[14px] text-[12px] font-normal uppercase border border-[#E0E0E0] text-[#575757]", className)}
+        {...props}
+    >
+        {children}
+    </button>
+  )
+}
+
+export default Button
