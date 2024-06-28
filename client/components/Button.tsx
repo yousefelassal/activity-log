048e4691bc8e9d099db86fe542ce66496742eb02
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-28 12:09:39 +0300

    refactor: Update Button component styling for hover and active states

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils"
 const Button = ({ children, className, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
     <button
-        className={cn("flex items-center justify-center gap-1  h-[45px] px-[14px] text-[12px] font-normal uppercase border border-[#E0E0E0] text-[#575757]", className)}
+        className={cn("flex items-center justify-center gap-1 h-[45px] px-[14px] text-[12px] font-normal uppercase border border-[#E0E0E0] text-[#575757] hover:bg-gray-700/5 active:scale-95 transition-all", className)}
         {...props}
     >
         {children}
